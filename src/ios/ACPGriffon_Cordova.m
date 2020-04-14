4e08151672577e524b31b20bb27591a62846eeca
steve benedick <sbenedic@adobe.com>
2020-04-14 15:41:57 -0600

    -migrate to public github

---
+++ src/ios/ACPGriffon_Cordova.m
@@ -0,0 +1,51 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+/********* ACPGriffon_Cordova.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+#import <ACPGriffon/ACPGriffon.h>
+
+@interface ACPGriffon_Cordova : CDVPlugin
+
+- (void)extensionVersion:(CDVInvokedUrlCommand*)command;
+- (void)startSession:(CDVInvokedUrlCommand*)command;
+
+@end
+
+@implementation ACPGriffon_Cordova
+
+- (void)extensionVersion:(CDVInvokedUrlCommand*)command
+{
+    [self.commandDelegate runInBackground:^{
+        CDVPluginResult* pluginResult = nil;
+        NSString* extensionVersion = [ACPGriffon extensionVersion];
+
+        if (extensionVersion != nil && [extensionVersion length] > 0) {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:extensionVersion];
+        } else {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        }
+        
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+- (void)startSession:(CDVInvokedUrlCommand*)command
+{
+    [self.commandDelegate runInBackground:^{
+        NSURL* url = [NSURL URLWithString:[command.arguments objectAtIndex:0]];
+        [ACPGriffon startSession:url];
+        [self.commandDelegate sendPluginResult:nil callbackId:command.callbackId];
+    }];
+}
+
+@end
